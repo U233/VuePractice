a8d1dfc3e02a2c964110b35db8443eb712fd5e5d
U233 <407036632@qq.com>
2020-10-21 00:18:44 +0800

    完成用户列表功能的开发

--- src/components/Home.vue
+++ src/components/Home.vue
@@ -17,7 +17,7 @@
         <!-- 侧边栏菜单区 -->
         <el-menu background-color="#333744" text-color="#fff"
           active-text-color="#409bff" :unique-opened="true" :collapse="isCollapse"
-          :collapse-transition="false">
+          :collapse-transition="false" :router="true" :default-active="activePath">
           <!-- 一级菜单 -->
           <el-submenu :index="item.id+''" v-for="item in menuList" :key="item.id">
             <!-- 一级菜单的模板区域 -->
@@ -28,7 +28,8 @@
               <span> {{item.authName}} </span>
             </template>
             <!-- 二级菜单 -->
-            <el-menu-item :index="''+subItem.id" v-for="subItem in item.children" :key="subItem.id">
+            <el-menu-item :index="'/'+subItem.path" v-for="subItem in item.children" :key="subItem.id"
+              @click="saveNavState('/'+subItem.path)">
               <template slot="title">
                 <!-- 图标 -->
                 <i class="el-icon-menu"></i>
@@ -40,7 +41,10 @@
         </el-menu>
       </el-aside>
       <!-- 右侧主题 -->
-      <el-main>Main</el-main>
+      <el-main>
+        <!-- 路由占位符 -->
+        <router-view></router-view>
+      </el-main>
     </el-container>
   </el-container>
 </template>
@@ -59,11 +63,14 @@ export default {
         145: 'el-icon-s-marketing'
       },
       // 是否折叠菜单
-      isCollapse: false
+      isCollapse: false,
+      // 被激活的链接地址
+      activePath: ''
     }
   },
   created() {
     this.getMenuList()
+    this.activePath = window.sessionStorage.getItem('activePath')
   },
   methods: {
     logout() {
@@ -75,11 +82,15 @@ export default {
       const { data: res } = await this.$http.get('menus')
       if (res.meta.status !== 200) return this.$message.error(res.meta.msg)
       this.menuList = res.data
-      console.log(res)
     },
     // 点击按钮切换菜单的折叠与展开
     toggleCollapse() {
       this.isCollapse = !this.isCollapse
+    },
+    // 保存链接的激活状态
+    saveNavState(activePath) {
+      window.sessionStorage.setItem('activePath', activePath)
+      this.activePath = activePath
     }
   }
 }
